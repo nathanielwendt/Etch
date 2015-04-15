78acc28d4986c5a817e300bc890bbe0162eadd7b
Nate Wendt <nwendt22@gmail.com>
2015-04-14 19:51:02 -0500

    sampled values from datalines table instead of application_state_statistics_x since datalines looks to be more consistent

--- scripts/sql.sh
+++ scripts/sql.sh
@@ -9,7 +9,7 @@ sqlite3 $1 <<!
 select '$2', IFNULL(sensor_name, 'application_state'), timestamp, value, delta, marker_time, description
 FROM
 
-(select '$2', (select name from application_state_statistics_1 WHERE _id=substr('$2',8,3)) as sensor_name,
+(select '$2', (select name from datalines WHERE _id=substr('$2',8,3)) as sensor_name,
             timestamp, value, delta, end_time-start_time as marker_time, description from
 $2,
 (select st_st as end_time, et_st as start_time, description
